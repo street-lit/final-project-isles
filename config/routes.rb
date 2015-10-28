a630ee3d808a2a9ff34328eef7035a59dd17745c
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-10-28 11:14:32 -0500

    Change logout route to not use jquery and manually route logout path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   get '/about',         to: "landing_pages#about", as: "about"
 
-  get     '/login',     to: 'sessions#new',        as: 'new_login'
-  post    '/login',     to: 'sessions#create',     as: 'login'
-  delete  '/logout',    to: 'sessions#destroy',    as: 'logout'
+  # get     '/login',     to: 'sessions#new',        as: 'new_login'
+  # post    '/login',     to: 'sessions#create',     as: 'login'
+  # get     '/logout',    to: 'sessions#destroy',    as: 'logout'
   get     '/home',      to: "users#home",          as: "home"
 
   resources :users
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
   resources :posts
   resources :comments
 
-  root "users#new"
+  controller :sessions do
+    get 'login' => :new
+    post 'login'=> :create
+    get 'logout' => :destroy
+  end
+
+  root "landing_pages#about"
 end
